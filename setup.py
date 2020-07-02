146428501cf2f8f5bdaaf3a03b9ccc64d191b29d
RealPolitiX <xrpatrick@gmail.com>
2020-07-02 18:15:15 -0500

    Updated the version to 0.6.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name="xany",
-    version="0.6.0",
+    version="0.6.2",
     url="https://github.com/RealPolitiX/symmetrize",
 
     author="R. Patrick Xian",
