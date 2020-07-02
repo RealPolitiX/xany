c72dcad66f1ac52e95748d76b42729daf82fabf6
RealPolitiX <xrpatrick@gmail.com>
2020-07-02 09:12:15 -0500

    Updated version number to 0.6.0

--- xany/__init__.py
+++ xany/__init__.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-from . import prep, utils, process
+from . import prep, utils, process, stitch
 import warnings
 
-__version_info__ = ('0', '5', '0')
+__version_info__ = ('0', '6', '0')
 __version__ = '.'.join(__version_info__)
 
 def fxn():
